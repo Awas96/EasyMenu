68ff243f09a66755c863c493b034f095dff2a549
Awas <awas831996@gmail.com>
2021-04-07 20:02:59 +0200

    Añadidas nuevas librerias de bootstrap

--- public/js/toggler.js
+++ public/js/toggler.js
@@ -1,6 +1,7 @@
 $("document").ready(toggle);
 // Este javascript selecciona los nodos que tienen la clase toggle y ponen/quitan la clase escondido
 function toggle() {
+    $('[data-toggle="tooltip"]').tooltip()
     $(".toggler").each(function () {
         let objeto = $(this);
         $(this).parent().hover(function () {
