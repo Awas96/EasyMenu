3d465d26557b507245b59e9a781e4831dd808bcd
Awas <awas831996@gmail.com>
2021-04-08 13:58:54 +0200

    Arreglada web para mostrar alergenos cuando pinchas un elemento

--- public/js/toggler.js
+++ public/js/toggler.js
@@ -1,11 +1,21 @@
 $("document").ready(toggle);
+
 // Este javascript selecciona los nodos que tienen la clase toggle y ponen/quitan la clase escondido
 function toggle() {
-    $('[data-toggle="tooltip"]').tooltip()
-    $(".toggler").each(function () {
-        let objeto = $(this);
-        $(this).parent().hover(function () {
-            objeto.toggleClass("esconder");
-        })
-    })
+    let elementos = $('.toggler');
+
+    elementos.each(function () {
+        let elemento = $(this);
+        let tooltip = $(this).children().last()
+        tooltip.hide()
+        $(this).parent().click(function () {
+            if (tooltip.is(':visible')) {
+                tooltip.hide(600);
+            } else {
+                tooltip.show(600);
+            }
+        });
+    });
+
+
 }
