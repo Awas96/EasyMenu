6f04eb35f1d59edf4117413831f278be179d04a3
Awas <awas831996@gmail.com>
2021-04-12 20:37:43 +0200

    Creado altas, bajas y modificaciones de  Elemento

--- src/Controller/ElementoController.php
+++ src/Controller/ElementoController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Elemento;
+use App\Form\ElementoType;
 use App\Repository\ElementoRepository;
+use App\Repository\SeccionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,4 +24,57 @@ class ElementoController extends AbstractController
             'elementos' => $elemento,
         ]);
     }
+
+    /**
+     * @Route("/elemento/{sec}/nuevo/", name="elemento_nuevo")
+     * @Route("/elemento/modificar/{id}", name="elemento_modificar")
+     */
+    public function gestionarElementos(Request $request, Elemento $elemento = null, SeccionRepository $seccionRepository, $sec = null): Response
+    {
+
+        if (null == $elemento) {
+            $elemento = new Elemento();
+            $elemento->setSeccion($seccionRepository->findById($sec));
+            $this->getDoctrine()->getManager()->persist($elemento);
+        } else {
+            $sec = $seccionRepository->findById($elemento->getSeccion())->getId();
+
+        }
+        $form = $this->createForm(ElementoType::class, $elemento);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute('indexSec', [
+                'sec' => $sec
+            ]);
+        }
+
+        return $this->render('elemento/modificar.partial.html.twig', [
+            'form' => $form->createView(),
+            'elemento' => $elemento,
+
+        ]);
+    }
+
+    /**
+     * @Route("/elemento/{sec}/eliminar/{id}", name="elemento_eliminar")
+     */
+    public function eliminar(Request $request, Elemento $elemento, $id, $sec): Response
+    {
+        if ($request->request->has('confirmar')) {
+            $this->getDoctrine()->getManager()->remove($elemento);
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute('indexSec', [
+                'sec' => $sec
+            ]);
+        }
+
+        return $this->render('elemento/eliminar.html.twig', [
+            'elemento' => $elemento
+        ]);
+    }
+
+
 }
