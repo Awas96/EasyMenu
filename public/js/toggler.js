c51089f97dccc78bc07585e7fc8fb143b60ad725
Awas <awas831996@gmail.com>
2021-04-12 14:03:01 +0200

    Comentados los elementos de public para entender su contenido

--- public/js/toggler.js
+++ public/js/toggler.js
@@ -1,6 +1,6 @@
 $("document").ready(toggle);
 
-// Este javascript selecciona los nodos que tienen la clase toggle y ponen/quitan la clase escondido
+// Este javascript selecciona los nodos que tienen la clase toggle y poner/quitan la clase escondido
 function toggle() {
     let elementos = $('.toggler');
 
@@ -8,6 +8,8 @@ function toggle() {
         let elemento = $(this);
         let trSpoiler = $('.spoiler')[index];
         elemento.parent().click(function () {
+            //Por el funcionamiento de symfony, hay que disponer de los elementos viuales
+            //de tal manera que haya que esconder al padre y al elemento para mostrarlo.
             if ($(trSpoiler))
                 if ($(trSpoiler).is(':visible')) {
                     $(trSpoiler).parent().hide(600);
