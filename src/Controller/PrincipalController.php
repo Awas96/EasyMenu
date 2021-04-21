281d5f0e37cb032db102c7720d8c6c2106b3be08
Awas <awas831996@gmail.com>
2021-04-21 19:46:52 +0200

    Creado generador de PDF

--- src/Controller/PrincipalController.php
+++ src/Controller/PrincipalController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Repository\ElementoRepository;
 use App\Repository\SeccionRepository;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -34,5 +36,42 @@ class PrincipalController extends AbstractController
             'nSeccion' => $sec,
             'elementos' => $elemento
         ]);
+    }
+
+    /**
+     * @Route("/GeneraPDF/{sec}", name="genera_pdf")
+     */
+    public function generate_pdf(SeccionRepository $seccionRepository, ElementoRepository $elementoRepository, $sec)
+    {
+
+        $options = new Options();
+        $options->set('defaultFont', 'Roboto');
+        $secciones = $seccionRepository->findAllOrderBy();
+        $elemento = $elementoRepository->findSecOrderBy($sec);
+
+        $dompdf = new Dompdf($options);
+
+        $data = array(
+            'headline' => 'xDDD'
+        );
+        $html = $this->renderView('pdf/pdf.html.twig', [
+            'headline' => "Test pdf generator",
+            "Attachment" => true,
+            'secciones' => $secciones,
+            'nSeccion' => $sec,
+            'elementos' => $elemento
+        ]);
+
+
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+        $dompdf->stream("testpdf.pdf", [
+            "Attachment" => true,
+            'secciones' => $secciones,
+            'nSeccion' => $sec,
+            'elementos' => $elemento
+        ]);
+
     }
 }
