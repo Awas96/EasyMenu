a5c0481deb7b1bfb93d34d2615f66a4ea53aedb9
Awas <awas831996@gmail.com>
2021-04-29 14:02:09 +0200

    Creada interfaz de administrador para gestiones de usuarios y pdfs

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -33,4 +33,13 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    /**
+     * @Route("/Admin", name="app_panel")
+     */
+    public function panelAdmin()
+    {
+        return $this->render('security/panel_administrador.html.twig', [
+        ]);
+    }
 }
