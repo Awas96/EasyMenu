a3430258bb4c08f6d9f2848f5dc4e27d1b510798
Awas <awas831996@gmail.com>
2021-05-10 12:10:04 +0200

    Separados formularios de user en varios tipos

--- src/Form/UserNameType.php
+++ src/Form/UserNameType.php
@@ -3,24 +3,19 @@
 namespace App\Form;
 
 use App\Entity\User;
+use phpDocumentor\Reflection\Types\String_;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UserType extends AbstractType
+class UserNameType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('password', RepeatedType::class, [
-                'type' => PasswordType::class,
-                'invalid_message' => 'Las contraseñas deben coincidir',
-                'required' => true,
-                'first_options' => ['label' => 'Contraseña'],
-                'second_options' => ['label' => 'Repetir Contraseña'],
-            ]);
+            ->add('username');
     }
 
     public function configureOptions(OptionsResolver $resolver)
---
+++ src/Form/UserPasswordType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Las contraseñas deben coincidir',
+                'required' => true,
+                'first_options' => ['label' => 'Contraseña'],
+                'second_options' => ['label' => 'Repetir Contraseña'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
